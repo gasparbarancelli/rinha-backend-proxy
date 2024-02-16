361be8e299cb57d1c9f8b0292d20f583c400ed5f
gasparbarancelli <gasparbarancelli@gmail.com>
2024-02-16 19:11:08 -0300

    proxy para rinha backend

--- src/main/java/io/gasparbarancelli/Proxy.java
+++ src/main/java/io/gasparbarancelli/Proxy.java
@@ -6,20 +6,38 @@ import io.undertow.server.handlers.proxy.ProxyHandler;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Optional;
 
 public class Proxy {
 
     public static void main(String[] args) throws URISyntaxException {
-        LoadBalancingProxyClient loadBalancer = new LoadBalancingProxyClient()
-                .addHost(new URI("http://localhost:8081"))
-                .addHost(new URI("http://localhost:8082"))
-                .setConnectionsPerThread(20);
+        var uris = Optional.ofNullable(System.getenv("uris"));
+        if (uris.isPresent()) {
+            var connectionsPerThread = Optional.ofNullable(System.getenv("connections-per-thread"))
+                    .map(Integer::parseInt).orElse(20);
 
-        Undertow reverseProxy = Undertow.builder()
-                .addHttpListener(9999, "localhost")
-                .setIoThreads(4)
-                .setHandler(ProxyHandler.builder().setProxyClient(loadBalancer).setMaxRequestTime( 30000).build())
-                .build();
-        reverseProxy.start();
+            LoadBalancingProxyClient loadBalancer = new LoadBalancingProxyClient()
+                    .setConnectionsPerThread(connectionsPerThread);
+
+            for (String uri : uris.get().split(",")) {
+                loadBalancer.addHost(new URI(uri));
+            }
+
+            var host = Optional.ofNullable(System.getenv("host"))
+                    .orElse("localhost");
+
+            var port = Optional.ofNullable(System.getenv("port"))
+                    .map(Integer::parseInt).orElse(9999);
+
+            var ioThreads = Optional.ofNullable(System.getenv("io-threads"))
+                    .map(Integer::parseInt).orElse(4);
+
+            Undertow reverseProxy = Undertow.builder()
+                    .addHttpListener(port, host)
+                    .setIoThreads(ioThreads)
+                    .setHandler(ProxyHandler.builder().setProxyClient(loadBalancer).setMaxRequestTime(30000).build())
+                    .build();
+            reverseProxy.start();
+        }
     }
 }
